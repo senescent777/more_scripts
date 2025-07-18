05ed1b8986f90bbbff5366573f33ae934d6217c0
senescent777 <113385047+senescent777@users.noreply.github.com>
2025-07-19 01:58:17 +0300

    Delete backup09/home/backupuser/bin directory

--- backup09/home/backupuser/bin/common.sh
+++
@@ -1,8 +0,0 @@
-backuppart=/media/backup #osio mille tallennetaan varmuskopiot ja mikä täytyy mountata sitä varten
-datestr=$(date +%F)
-vgname=vg1 #volume group:in nimi mikäli $backuppart on LVM:n takana
-XFSDUMP_CMD="sudo /usr/sbin/xfsdump" # $(which xfsdump)
-VGCHANGE_CMD="sudo /sbin/vgchange" # $(which vgchange)
-MOUNT_CMD=$(which mount) #tämä ja seuraava sitä varten jos mount:ailu vaadi sudon
-UMOUNT_CMD=$(which umount)
-EXT3OPTS=" --exclude lost+found -jcvpf " #taria varten(ext3 toistaiseksi tar:illa)
